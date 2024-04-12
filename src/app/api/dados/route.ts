45d3112a4f068d6965b81213c84eb0114ef06616
wesuRage <weslley.nmiranda@gmail.com>
2024-04-12 20:50:12 -0300

    API pronta

--- src/app/api/dados/route.ts
+++ src/app/api/dados/route.ts
@@ -1,9 +1,26 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma";
+import { connectToDatabase } from "@/helpers/connectToDatabase";
 
 export async function POST(request: Request) {
   try {
     const { planta } = await request.json();
+
+    await connectToDatabase();
+
+    prisma.safra.create({
+      data: {
+        planta,
+        umidade: 0,
+        temperatura: 0,
+        solo: 0,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Planta adicionada com sucesso" },
+      { status: 201 }
+    );
   } catch (error) {
     console.log(error);
 
@@ -16,6 +33,20 @@ export async function POST(request: Request) {
 export async function PUT(request: Request) {
   try {
     const { planta, umidade, temperatura, solo } = await request.json();
+
+    await connectToDatabase();
+
+    await prisma.safra.update({
+      where: {
+        planta,
+      },
+      data: {
+        planta,
+        umidade,
+        temperatura,
+        solo,
+      },
+    });
   } catch (error) {
     console.log(error);
 
@@ -27,6 +58,11 @@ export async function PUT(request: Request) {
 
 export async function GET() {
   try {
+    await connectToDatabase();
+
+    const data = await prisma.safra.findMany();
+
+    return NextResponse.json({ data }, { status: 200 });
   } catch (error) {
     console.log(error);
 
