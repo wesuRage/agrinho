82145cb0dc0a4b19351c1601cb39ad276f7683fb
wesuRage <weslley.nmiranda@gmail.com>
2024-04-14 01:34:14 -0300

    resolvendo CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  async headers() {
     return [
       {
         source: "/api/:path*",
-        destination: "https://esp32-agrinho.vercel.app/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE" },
+        ],
       },
     ];
   },
